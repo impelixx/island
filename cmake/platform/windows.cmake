d4672f07634eab168c90e66f1eb084981b0ce5d7
Anton <impelix@icloud.com>
2026-08-08 17:50:33 +0300

    Add Windows CEF bootstrap
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

---
+++ cmake/platform/windows.cmake
@@ -0,0 +1,47 @@
+function(island_add_windows_browser_target)
+    if(NOT WIN32)
+        message(FATAL_ERROR "island_add_windows_browser_target requires Windows.")
+    endif()
+
+    set(CEF_TARGET island_browser)
+    SET_CEF_TARGET_OUT_DIR()
+
+    ADD_LOGICAL_TARGET(libcef_lib "${CEF_LIB_DEBUG}" "${CEF_LIB_RELEASE}")
+    if(NOT TARGET libcef_dll_wrapper)
+        message(FATAL_ERROR
+            "island_add_windows_browser_target requires libcef_dll_wrapper before inclusion.")
+    endif()
+
+    if(NOT TARGET island_browser_core)
+        message(FATAL_ERROR
+            "island_add_windows_browser_target requires island_browser_core before inclusion.")
+    endif()
+
+    set(_island_windows_source_dir "${CMAKE_CURRENT_FUNCTION_LIST_DIR}/../../src/main/windows")
+    if(USE_SANDBOX)
+        add_library(${CEF_TARGET} SHARED "${_island_windows_source_dir}/main_win.cc")
+        SET_LIBRARY_TARGET_PROPERTIES(${CEF_TARGET})
+        COPY_SINGLE_FILE(${CEF_TARGET} "${CEF_BINARY_DIR}/bootstrap.exe"
+            "${CEF_TARGET_OUT_DIR}/${CEF_TARGET}.exe")
+
+        if(CMAKE_GENERATOR MATCHES "Visual Studio")
+            set_target_properties(${CEF_TARGET} PROPERTIES
+                VS_DEBUGGER_COMMAND "$<TARGET_FILE_DIR:${CEF_TARGET}>/${CEF_TARGET}.exe")
+        endif()
+    else()
+        add_executable(${CEF_TARGET} WIN32 "${_island_windows_source_dir}/main_win.cc")
+        SET_EXECUTABLE_TARGET_PROPERTIES(${CEF_TARGET})
+        ADD_WINDOWS_MANIFEST("${_island_windows_source_dir}" "${CEF_TARGET}" "exe")
+    endif()
+    target_compile_features(${CEF_TARGET} PRIVATE cxx_std_20)
+    target_link_libraries(${CEF_TARGET} PRIVATE
+        libcef_lib
+        island_browser_core
+        ${CEF_STANDARD_LIBS}
+    )
+    COPY_FILES("${CEF_TARGET}" "${CEF_BINARY_FILES}" "${CEF_BINARY_DIR}" "${CEF_TARGET_OUT_DIR}")
+    COPY_FILES("${CEF_TARGET}" "${CEF_RESOURCE_FILES}" "${CEF_RESOURCE_DIR}" "${CEF_TARGET_OUT_DIR}")
+    if(USE_SANDBOX)
+        SET_LPAC_ACLS("${CEF_TARGET}")
+    endif()
+endfunction()
---
+++ src/main/windows/main_win.cc
@@ -0,0 +1,75 @@
+#include <shellapi.h>
+#include <windows.h>
+
+#include <array>
+#include <span>
+#include <string_view>
+#include <utility>
+
+#include "../app_runtime.h"
+#include "../island_app.h"
+#include "../startup_options.h"
+#include "include/cef_app.h"
+#include "include/cef_sandbox_win.h"
+
+namespace {
+
+island::StartupOptions ParseStartupOptions() {
+    int argument_count = 0;
+    wchar_t** arguments = CommandLineToArgvW(GetCommandLineW(), &argument_count);
+    if (arguments == nullptr) {
+        return island::StartupOptions::Parse({});
+    }
+
+    bool is_smoke_test = false;
+    for (int index = 0; index < argument_count; ++index) {
+        if (std::wstring_view(arguments[index]) == L"--island-smoke-test") {
+            is_smoke_test = true;
+            break;
+        }
+    }
+
+    LocalFree(arguments);
+
+    if (!is_smoke_test) {
+        return island::StartupOptions::Parse({});
+    }
+
+    constexpr std::array<std::string_view, 1> smoke_test_arguments = {"--island-smoke-test"};
+    return island::StartupOptions::Parse(smoke_test_arguments);
+}
+
+int RunMain(HINSTANCE instance, void* sandbox_info) {
+    CefMainArgs main_args(instance);
+    const island::StartupOptions startup_options = ParseStartupOptions();
+    CefRefPtr<island::IslandApp> app(new island::IslandApp(startup_options));
+
+    const int exit_code = CefExecuteProcess(main_args, app, sandbox_info);
+    if (exit_code >= 0) {
+        return exit_code;
+    }
+
+    return island::RunIslandMainProcess(main_args, std::move(app), nullptr, sandbox_info);
+}
+
+}  // namespace
+
+#if defined(CEF_USE_BOOTSTRAP)
+
+CEF_BOOTSTRAP_EXPORT int RunWinMain(HINSTANCE instance, LPTSTR, int, void* sandbox_info,
+                                    cef_version_info_t*) {
+    return RunMain(instance, sandbox_info);
+}
+
+#else
+
+int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE, LPTSTR, int) {
+    void* sandbox_info = nullptr;
+#if defined(CEF_USE_SANDBOX)
+    CefScopedSandboxInfo scoped_sandbox;
+    sandbox_info = scoped_sandbox.sandbox_info();
+#endif
+    return RunMain(instance, sandbox_info);
+}
+
+#endif
